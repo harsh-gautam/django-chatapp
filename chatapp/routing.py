9891491658644e1500f3f09b58c24e984371fe5d
Harsh Gautam <hgautam208@gmail.com>
2021-07-20 22:30:03 +0530

    fixed bug where notification consumer connects on every url

--- chatapp/routing.py
+++ chatapp/routing.py
@@ -2,7 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from django.urls import re_path
+from django.urls import re_path, path
 
 from chat.consumers import ChatRoomConsumer
 from private_chat.consumers import PrivateChatConsumer
@@ -14,7 +14,7 @@ application = ProtocolTypeRouter(
           AuthMiddlewareStack(
               URLRouter(
                 [
-                  re_path(r'', NotificationConsumer.as_asgi()),
+                  path('', NotificationConsumer.as_asgi()),
                   re_path(
                     r"^ws/chat/room/(?P<room_title>\w+)/$",
                     ChatRoomConsumer.as_asgi(),
